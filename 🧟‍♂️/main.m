65ced90fd3e14acfe022ed1e2456d9460a6bdb8c
Kam <Kamtoto.au@gmail.com>
2020-08-19 19:45:08 +0800

    Initial Commit

---
+++ 🧟‍♂️/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  🧟‍♂️
+//
+//  Created by Kam on 2020/8/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
